5aed797bfe6690820feae8e4b0b196a340a1346a
Christian Auth <c.auth@syzygy.de>
2017-05-30 17:49:13 +0200

    chore: remove chai-as-promised

--- test/configuration.js
+++ test/configuration.js
@@ -21,9 +21,6 @@ const chai = require('chai');
 chai.config.includeStack = true;
 global.expect = chai.expect;
 
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-
 const chaiSubset = require('chai-subset');
 chai.use(chaiSubset);
 
