0a70e19d1910f546ac3a7d5f1d5a697becbd560c
Christian Auth <c.auth@syzygy.de>
2017-06-07 11:52:45 +0200

    feat(nunjucks.filter.LinkUrlFilter): adds a filter for link generation

---
+++ source/nunjucks/filter/LinkUrlFilter.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const Filter = require('./Filter.js').Filter;
+const isPlainObject = require('lodash.isplainobject');
+
+
+/**
+ * Generates a link url fro the given link object.
+ * This serves as a integration helper for cms's
+ *
+ * @memberOf nunjucks.filter
+ */
+class LinkUrlFilter extends Filter
+{
+    /**
+     * @inheritDoc
+     */
+    constructor(dataProperties)
+    {
+        super();
+        this._name = 'linkUrl';
+
+        // Assign options
+        this.dataProperties = dataProperties || [];
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    static get injections()
+    {
+        return { 'parameters': ['nunjucks.filter/LinkUrlFilter.dataProperties'] };
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    static get className()
+    {
+        return 'nunjucks.filter/LinkUrlFilter';
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    filter()
+    {
+        const scope = this;
+        return function(value)
+        {
+            if (typeof value === 'string')
+            {
+                return value;
+            }
+            if (isPlainObject(value))
+            {
+                for (const dataProperty of scope.dataProperties)
+                {
+                    if (typeof value[dataProperty] === 'string')
+                    {
+                        return value[dataProperty];
+                    }
+                }
+            }
+            return 'JavaScript:;';
+        };
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.LinkUrlFilter = LinkUrlFilter;
---
+++ test/nunjucks/filter/LinkUrlFilterSpec.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Requirements
+ */
+const LinkUrlFilter = require(ES_SOURCE + '/nunjucks/filter/LinkUrlFilter.js').LinkUrlFilter;
+const filterSpec = require(ES_TEST + '/nunjucks/filter/FilterShared.js').spec;
+
+
+/**
+ * Spec
+ */
+describe(LinkUrlFilter.className, function()
+{
+    /**
+     * Filter Test
+     */
+    filterSpec(LinkUrlFilter, 'nunjucks.filter/LinkUrlFilter');
+
+
+    /**
+     * LinkUrlFilter Test
+     */
+    describe('#filter()', function()
+    {
+        it('should return a "empty" link when no valid link given', function()
+        {
+            const testee = new LinkUrlFilter().filter();
+            expect(testee()).to.be.equal('JavaScript:;');
+            expect(testee(1)).to.be.equal('JavaScript:;');
+            expect(testee(true)).to.be.equal('JavaScript:;');
+            expect(testee({})).to.be.equal('JavaScript:;');
+        });
+
+        it('should return a link when given a string', function()
+        {
+            const testee = new LinkUrlFilter().filter();
+            expect(testee('/hi/there')).to.be.equal('/hi/there');
+        });
+
+        it('should allow to configure properties to search for a link when given a object', function()
+        {
+            const testee = new LinkUrlFilter(['url']).filter();
+            expect(testee({ url: '/hi/there' })).to.be.equal('/hi/there');
+        });
+    });
+});
