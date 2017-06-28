1d8220c04fbc3c8fefd08de28d38947c73e8f180
Christian Auth <c.auth@syzygy.de>
2017-06-28 19:27:29 +0200

    feat(command.LintCommand): adds command to lint entitites

---
+++ source/command/LintCommand.js
@@ -0,0 +1,224 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const Command = require('./Command.js').Command;
+const Context = require('../application/Context.js').Context;
+const GlobalRepository = require('../model/GlobalRepository.js').GlobalRepository;
+const PathesConfiguration = require('../model/configuration/PathesConfiguration.js').PathesConfiguration;
+const assertParameter = require('../utils/assert.js').assertParameter;
+const ErrorHandler = require('../error/ErrorHandler.js').ErrorHandler;
+const chalk = require('chalk');
+const co = require('co');
+
+
+/**
+ * @memberOf command
+ */
+class LintCommand extends Command
+{
+    /**
+     */
+    constructor(context, globalRepository, pathesConfiguration, linters, options)
+    {
+        super(context);
+
+        //Check params
+        assertParameter(this, 'globalRepository', globalRepository, true, GlobalRepository);
+        assertParameter(this, 'pathesConfiguration', pathesConfiguration, true, PathesConfiguration);
+
+        // Assign options
+        this._name = 'lint';
+        this._globalRepository = globalRepository;
+        this._pathesConfiguration = pathesConfiguration;
+        this._linters = linters || [];
+        this._options = options || {};
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    static get injections()
+    {
+        return { 'parameters': [Context, GlobalRepository, PathesConfiguration, 'command/LintCommand.linters', 'command/LintCommand.options'] };
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    static get className()
+    {
+        return 'command/LintCommand';
+    }
+
+
+    /**
+     * @type {Object}
+     */
+    get options()
+    {
+        return this._options;
+    }
+
+
+    /**
+     * @type {Array}
+     */
+    get linters()
+    {
+        return this._linters;
+    }
+
+
+    /**
+     * @type {model.configuration.PathesConfiguration}
+     */
+    get pathesConfiguration()
+    {
+        return this._pathesConfiguration;
+    }
+
+
+    /**
+     * @type {model.GlobalRepository}
+     */
+    get globalRepository()
+    {
+        return this._globalRepository;
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    get help()
+    {
+        const help =
+        {
+            name: this._name,
+            description: 'Lints files',
+            actions: []
+        };
+        return help;
+    }
+
+
+    /**
+     * @inheritDocs
+     * @returns {Promise<Server>}
+     */
+    lint(action, parameters)
+    {
+        const scope = this;
+        const logger = scope.createLogger('command.lint');
+        const promise = co(function *()
+        {
+            const query = action || '*';
+            const section = logger.section('Linting <' + query + '>');
+            const sectionResult =
+            {
+                errorCount: 0,
+                warningCount: 0
+            };
+
+            const entities = yield scope.globalRepository.resolveEntities(query);
+            for (const entity of entities)
+            {
+                const entityPath = yield scope.pathesConfiguration.resolveEntityForSite(entity.entity, entity.site);
+                const result =
+                {
+                    success: true,
+                    errorCount: 0,
+                    warningCount: 0,
+                    messages: [],
+                    files: []
+                };
+                const work = logger.work();
+                for (const linter of scope.linters)
+                {
+                    const linterResult = yield linter.lint(entityPath);
+                    if (!linterResult.success)
+                    {
+                        result.success = false;
+                    }
+                    result.errorCount+= linterResult.errorCount;
+                    result.warningCount+= linterResult.warningCount;
+                    sectionResult.errorCount+= linterResult.errorCount;
+                    sectionResult.warningCount+= linterResult.warningCount;
+                    Array.prototype.push.apply(result.messages, linterResult.messages);
+                    Array.prototype.push.apply(result.files, linterResult.files);
+                }
+
+                if (result.success)
+                {
+                    logger.end(work, false, 'Linting <' + entity.id.asString('path') + '>');
+                }
+                else
+                {
+                    logger.end(work, true, 'Linting <' + entity.id.asString('path') + '> failed with ' +
+                        chalk.yellow(result.errorCount) + ' errors and ' +
+                        chalk.yellow(result.warningCount) + ' warnings.');
+
+                    //Show messages
+                    const messagesByFile = {};
+                    for (const message of result.messages)
+                    {
+                        messagesByFile[message.filename] = messagesByFile[message.filename] || [];
+                        messagesByFile[message.filename].push(message);
+                    }
+                    for (const file in messagesByFile)
+                    {
+                        const messages = messagesByFile[file];
+                        logger.info('   ' + messages[0].filename.replace(scope._pathesConfiguration.root, '') + '');
+                        for (const message of messages)
+                        {
+                            logger.info(chalk.magenta('     @' + message.line) + ' ' + message.message + chalk.dim(' (' + message.ruleId + ')'));
+                        }
+                        logger.info();
+                    }
+                }
+            }
+
+            if (sectionResult.errorCount > 0)
+            {
+                // exit with non-zero for git hooks
+                logger.end(section, true);
+                if (scope.options.exitCodes !== false)
+                {
+                    process.exit(1);
+                }
+            }
+            else
+            {
+                // exit with zero
+                logger.end(section, false);
+                if (scope.options.exitCodes !== false)
+                {
+                    process.exit(0);
+                }
+            }
+        }).catch(ErrorHandler.handler(scope));
+        return promise;
+    }
+
+
+    /**
+     * @inheritDocs
+     * @returns {Promise<Server>}
+     */
+    dispatch(action, parameters)
+    {
+        return this.lint(action, parameters);
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.LintCommand = LintCommand;
---
+++ test/command/LintCommandSpec.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * Requirements
+ */
+const LintCommand = require(ES_SOURCE + '/command/LintCommand.js').LintCommand;
+const commandSpec = require(ES_TEST + '/command/CommandShared.js').spec;
+const projectFixture = require(ES_FIXTURES + '/project/index.js');
+const FileLinter = require(ES_SOURCE + '/linter/FileLinter.js').FileLinter;
+const co = require('co');
+const sinon = require('sinon');
+
+
+/**
+ * Spec
+ */
+describe(LintCommand.className, function()
+{
+    /**
+     * Command Test
+     */
+    commandSpec(LintCommand, 'command/LintCommand', prepareParameters);
+
+    // Adds necessary parameters to create a testee
+    function prepareParameters(parameters)
+    {
+        global.fixtures = projectFixture.createDynamic();
+        return [global.fixtures.context, global.fixtures.globalRepository, global.fixtures.pathesConfiguration, [], { exitCodes: false }];
+    }
+
+
+    /**
+     * LintCommand Test
+     */
+    beforeEach(function()
+    {
+        global.fixtures = projectFixture.createDynamic();
+    });
+
+    function createTestee(linters)
+    {
+        global.fixtures = projectFixture.createDynamic();
+        return new LintCommand(global.fixtures.context, global.fixtures.globalRepository, global.fixtures.pathesConfiguration, linters, { exitCodes: false });
+    }
+
+    describe('#execute', function()
+    {
+        it('should apply all linter to all entities', function()
+        {
+            const promise = co(function*()
+            {
+                const linter = new FileLinter();
+                sinon.spy(linter, 'lint');
+                const testee = createTestee([linter]);
+                yield testee.execute({ command: 'lint' });
+                expect(linter.lint.callCount).to.be.equal(18);
+            });
+            return promise;
+        });
+    });
+});
