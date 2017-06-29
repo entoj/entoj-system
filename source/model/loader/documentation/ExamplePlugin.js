cc64786c4f1cfc2ba40767134d65e74210585760
Christian Auth <c.auth@syzygy.de>
2017-06-29 09:31:05 +0200

    feat(model.loader.documentation.ExamplePlugin): adds example loader

--- source/model/loader/documentation/ExamplePlugin.js
+++ source/model/loader/documentation/ExamplePlugin.js
@@ -4,18 +4,14 @@
  * Requirements
  * @ignore
  */
-const LoaderPlugin = require('../LoaderPlugin.js').LoaderPlugin;
+const ParserPlugin = require('./ParserPlugin.js').ParserPlugin;
 const PathesConfiguration = require('../../configuration/PathesConfiguration.js').PathesConfiguration;
-const Entity = require('../../entity/Entity.js').Entity;
-const Site = require('../../site/Site.js').Site;
-const assertParameter = require('../../../utils/assert.js').assertParameter;
-const co = require('co');
-const fs = require('co-fs-extra');
+const ExampleFileParser = require('../../../parser/documentation/ExampleFileParser.js').ExampleFileParser;
 
 /**
- * Reads entity example files
+ * Reads example files
  */
-class PackagePlugin extends LoaderPlugin
+class ExamplePlugin extends ParserPlugin
 {
     /**
      * @param {configuration.PathesConfiguration} pathesConfiguration
@@ -23,15 +19,10 @@ class PackagePlugin extends LoaderPlugin
      */
     constructor(pathesConfiguration, options)
     {
-        super();
-
-        //Check params
-        assertParameter(this, 'pathesConfiguration', pathesConfiguration, true, PathesConfiguration);
+        super(pathesConfiguration);
 
         // Assign options
-        const opts = options || {};
-        this._pathesConfiguration = pathesConfiguration;
-        this._filename = opts.filename || '/entity.json';
+        this._parser = new ExampleFileParser(options);
     }
 
 
@@ -40,7 +31,7 @@ class PackagePlugin extends LoaderPlugin
      */
     static get injections()
     {
-        return { 'parameters': [PathesConfiguration, 'model.loader.documentation/PackagePlugin.options'] };
+        return { 'parameters': [PathesConfiguration, 'model.loader.documentation/ExamplePlugin.options'] };
     }
 
 
@@ -49,101 +40,7 @@ class PackagePlugin extends LoaderPlugin
      */
     static get className()
     {
-        return 'model.loader.documentation/PackagePlugin';
-    }
-
-
-    /**
-     * @property {string}
-     */
-    get filename()
-    {
-        return this._filename;
-    }
-
-
-    /**
-     * @param {DocumentableValueObject} item
-     */
-    executeFor(item, site)
-    {
-        // Guards
-        assertParameter(this, 'item', item, true, [Entity, Site]);
-        assertParameter(this, 'site', site, false, Site);
-
-        const scope = this;
-        const promise = co(function*()
-        {
-            // Get filename
-            let filename;
-            if (!site)
-            {
-                filename = yield scope._pathesConfiguration.resolve(item, scope.filename);
-            }
-            else
-            {
-                filename = yield scope._pathesConfiguration.resolveEntityForSite(item, site, scope.filename);
-            }
-
-            // Check file
-            const exists = yield fs.exists(filename);
-            /* istanbul ignore next */
-            if (!exists)
-            {
-                scope.logger.debug('Could not find file', filename);
-                return false;
-            }
-
-            // Read it
-            const content = yield fs.readFile(filename);
-            /* istanbul ignore next */
-            if (!content)
-            {
-                scope.logger.warn('Could not read file', filename);
-                return false;
-            }
-
-            // Parse it
-            let data = false;
-            try
-            {
-                data = JSON.parse(content);
-            }
-            catch(e)
-            {
-                /* istanbul ignore next */
-                scope.logger.warn('Could not parse file', filename, e);
-                /* istanbul ignore next */
-                return false;
-            }
-
-            // Add data to item
-            if (data)
-            {
-                // Get site
-                let s = site;
-                if (!s && item.id && item.id.site)
-                {
-                    s = item.id.site;
-                }
-
-                // Add namespaced
-                if (s)
-                {
-                    const namespacedData = {};
-                    namespacedData[s.name.toLowerCase()] = data;
-                    item.properties.load(namespacedData);
-                }
-                // Add straight
-                else
-                {
-                    item.properties.load(data);
-                }
-            }
-
-            return true;
-        });
-        return promise;
+        return 'model.loader.documentation/ExamplePlugin';
     }
 }
 
@@ -152,4 +49,4 @@ class PackagePlugin extends LoaderPlugin
  * Exports
  * @ignore
  */
-module.exports.PackagePlugin = PackagePlugin;
+module.exports.ExamplePlugin = ExamplePlugin;
