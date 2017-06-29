fced586b73cf532565b58cb0c575cb785fdc944e
Christian Auth <c.auth@syzygy.de>
2017-06-29 09:33:03 +0200

    test(model.loader.documentation.JinjaPlugin): adds tests

---
+++ test/model/loader/documentation/JinjaPluginSpec.js
@@ -0,0 +1,31 @@
+/**
+ * Requirements
+ */
+const JinjaPlugin = require(ES_SOURCE + '/model/loader/documentation/JinjaPlugin.js').JinjaPlugin;
+const projectFixture = require(ES_FIXTURES + '/project/index.js');
+const loaderPluginSpec = require('../LoaderPluginShared.js').spec;
+
+
+/**
+ * Spec
+ */
+describe(JinjaPlugin.className, function()
+{
+    /**
+     * LoaderPlugin Test
+     */
+    loaderPluginSpec(JinjaPlugin, 'model.loader.documentation/JinjaPlugin', function(params)
+    {
+        params.unshift(global.fixtures.pathesConfiguration);
+        return params;
+    });
+
+
+    /**
+     * JinjaPlugin Test
+     */
+    beforeEach(function()
+    {
+        global.fixtures = projectFixture.createStatic();
+    });
+});
