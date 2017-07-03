1f201c127b76a8784a1923e40f07beff6e53b670
Christian Auth <c.auth@syzygy.de>
2017-07-03 10:09:43 +0200

    chore: fixes gulp version

--- source/nunjucks/filter/index.js
+++ source/nunjucks/filter/index.js
@@ -2,19 +2,19 @@
 /**
  * @namespace nunjucks.filter
  */
- module.exports =
- {
-     AssetUrlFilter: require('./AssetUrlFilter.js').AssetUrlFilter,
-     AttributesFilter: require('./AttributesFilter.js').AttributesFilter,
-     EmptyFilter: require('./EmptyFilter.js').EmptyFilter,
-     Filter: require('./Filter.js').Filter,
-     LinkUrlFilter: require('./LinkUrlFilter.js').LinkUrlFilter,
-     LoadFilter: require('./LoadFilter.js').LoadFilter,
-     MarkdownFilter: require('./MarkdownFilter.js').MarkdownFilter,
-     MarkupFilter: require('./MarkupFilter.js').MarkupFilter,
-     MediaQueryFilter: require('./MediaQueryFilter.js').MediaQueryFilter,
-     ModuleClassesFilter: require('./ModuleClassesFilter.js').ModuleClassesFilter,
-     NotEmptyFilter: require('./NotEmptyFilter.js').NotEmptyFilter,
-     SettingFilter: require('./SettingFilter.js').SettingFilter,
-     TranslateFilter: require('./TranslateFilter.js').TranslateFilter
- };
+module.exports =
+{
+    AssetUrlFilter: require('./AssetUrlFilter.js').AssetUrlFilter,
+    AttributesFilter: require('./AttributesFilter.js').AttributesFilter,
+    EmptyFilter: require('./EmptyFilter.js').EmptyFilter,
+    Filter: require('./Filter.js').Filter,
+    LinkUrlFilter: require('./LinkUrlFilter.js').LinkUrlFilter,
+    LoadFilter: require('./LoadFilter.js').LoadFilter,
+    MarkdownFilter: require('./MarkdownFilter.js').MarkdownFilter,
+    MarkupFilter: require('./MarkupFilter.js').MarkupFilter,
+    MediaQueryFilter: require('./MediaQueryFilter.js').MediaQueryFilter,
+    ModuleClassesFilter: require('./ModuleClassesFilter.js').ModuleClassesFilter,
+    NotEmptyFilter: require('./NotEmptyFilter.js').NotEmptyFilter,
+    SettingFilter: require('./SettingFilter.js').SettingFilter,
+    TranslateFilter: require('./TranslateFilter.js').TranslateFilter
+};
