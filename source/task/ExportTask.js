dfd7a9aff01d561d2209b52bb6095325abedd071
Christian Auth <c.auth@syzygy.de>
2017-07-05 19:03:52 +0200

    feat(task.ExportTask): adds export task

---
+++ source/task/ExportTask.js
@@ -0,0 +1,158 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const EntitiesTask = require('./EntitiesTask.js').EntitiesTask;
+const GlobalRepository = require('../model/GlobalRepository.js').GlobalRepository;
+const Exporter = require('../export/Exporter.js').Exporter;
+const CliLogger = require('../cli/CliLogger.js').CliLogger;
+const ErrorHandler = require('../error/ErrorHandler.js').ErrorHandler;
+const assertParameter = require('../utils/assert.js').assertParameter;
+const VinylFile = require('vinyl');
+const co = require('co');
+
+
+/**
+ * @memberOf task
+ */
+class ExportTask extends EntitiesTask
+{
+    /**
+     * @param {cli.CliLogger} cliLogger
+     * @param {model.GlobalRepository} globalRepository
+     * @param {export.Exporter} exporter
+     */
+    constructor(cliLogger, globalRepository, exporter)
+    {
+        super(cliLogger, globalRepository);
+
+        //Check params
+        assertParameter(this, 'exporter', exporter, true, Exporter);
+
+        // Assign options
+        this._exporter = exporter;
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    static get injections()
+    {
+        return { 'parameters': [CliLogger, GlobalRepository, Exporter] };
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    static get className()
+    {
+        return 'task/ExportTask';
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    get sectionName()
+    {
+        return 'Exporting files';
+    }
+
+
+    /**
+     * @type {String}
+     */
+    get exportName()
+    {
+        return 'default';
+    }
+
+
+    /**
+     * @type {export.Exporter}
+     */
+    get exporter()
+    {
+        return this._exporter;
+    }
+
+
+    /**
+     * @returns {Promise<VinylFile>}
+     */
+    renderEntity(entity, entitySettings, buildConfiguration, parameters)
+    {
+        if (!entity)
+        {
+            this.logger.warn(this.className + '::renderEntity - No entity given');
+            return Promise.resolve(false);
+        }
+
+        const scope = this;
+        const promise = co(function *()
+        {
+            // Prepare
+            const settings = entitySettings || {};
+            const macroName = settings.macro || entity.idString.lodasherize();
+            const siteName = entity.site.name;
+
+            // Export
+            const work = scope._cliLogger.work('Exporting macro <' + macroName + '>');
+            const exported = yield scope.exporter.export(siteName, macroName, settings);
+            scope._cliLogger.end(work);
+
+            // Done
+            const file = new VinylFile(
+                {
+                    path: exported.filename,
+                    contents: new Buffer(exported.contents)
+                });
+            return file;
+        }).catch(ErrorHandler.handler(scope));
+        return promise;
+    }
+
+
+    /**
+     * @returns {Promise<Array<VinylFile>>}
+     */
+    processEntity(entity, buildConfiguration, parameters)
+    {
+        /* istanbul ignore next */
+        if (!entity)
+        {
+            this.logger.warn(this.className + '::processEntity - No entity given');
+            return Promise.resolve(false);
+        }
+
+        const scope = this;
+        const promise = co(function *()
+        {
+            // Render each configured release
+            const result = [];
+            const settings = entity.properties.getByPath('export.' + scope.exportName, []);
+            for (const setting of settings)
+            {
+                // Render entity
+                const file = yield scope.renderEntity(entity, setting, buildConfiguration, parameters);
+                if (file)
+                {
+                    result.push(file);
+                }
+            }
+            return result;
+        }).catch(ErrorHandler.handler(scope));
+        return promise;
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.ExportTask = ExportTask;
---
+++ test/task/ExportTaskSpec.js
@@ -0,0 +1,132 @@
+'use strict';
+
+/**
+ * Requirements
+ */
+const ExportTask = require(ES_SOURCE + '/task/ExportTask.js').ExportTask;
+const Exporter = require(ES_SOURCE + '/export/Exporter.js').Exporter;
+const JinjaParser = require(ES_SOURCE + '/export/parser/JinjaParser.js').JinjaParser;
+const Renderer = require(ES_SOURCE + '/export/Renderer.js').Renderer;
+const AnyNodeRenderer = require(ES_SOURCE + '/export/renderer/AnyNodeRenderer.js').AnyNodeRenderer;
+const Transformer = require(ES_SOURCE + '/export/Transformer.js').Transformer;
+const projectFixture = require(ES_FIXTURES + '/project/index.js');
+const entitiesTaskSpec = require(ES_TEST + '/task/EntitiesTaskShared.js').spec;
+const co = require('co');
+const VinylFile = require('vinyl');
+const sinon = require('sinon');
+
+
+/**
+ * Spec
+ */
+describe(ExportTask.className, function()
+{
+    /**
+     * EntitiesTask Test
+     */
+    entitiesTaskSpec(ExportTask, 'task/ExportTask', function()
+    {
+        const fixture = projectFixture.createStatic();
+        const parser = new JinjaParser();
+        const renderer = new Renderer([new AnyNodeRenderer()]);
+        const transformer = new Transformer();
+        const exporter = new Exporter(fixture.globalRepository, fixture.buildConfiguration, parser, renderer, transformer);
+        return [fixture.cliLogger, fixture.globalRepository, exporter];
+    });
+
+
+    /**
+     * ExportTask Test
+     */
+    beforeEach(function()
+    {
+        global.fixtures = projectFixture.createDynamic();
+    });
+
+    // creates a initialized testee
+    const createTestee = function()
+    {
+        const parser = new JinjaParser();
+        const renderer = new Renderer([new AnyNodeRenderer()]);
+        const transformer = new Transformer();
+        const exporter = new Exporter(global.fixtures.globalRepository, global.fixtures.buildConfiguration, parser, renderer, transformer);
+        return new ExportTask(global.fixtures.cliLogger, global.fixtures.globalRepository, exporter);
+    };
+
+    describe('#renderEntity()', function()
+    {
+        it('should return a promise', function()
+        {
+            const testee = createTestee();
+            const promise = testee.renderEntity();
+            expect(promise).to.be.instanceof(Promise);
+            return promise;
+        });
+
+        it('should use exporter.export', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                sinon.spy(testee.exporter, 'export');
+                const entity = (yield global.fixtures.globalRepository.resolveEntities('base/modules/m-teaser')).pop();
+                yield testee.renderEntity(entity);
+                expect(testee.exporter.export.calledOnce).to.be.true;
+            });
+            return promise;
+        });
+
+        it('should yield a VinylFile', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                const entity = (yield global.fixtures.globalRepository.resolveEntities('base/modules/m-teaser')).pop();
+                const file = yield testee.renderEntity(entity);
+                expect(file).to.be.instanceof(VinylFile);
+                expect(file.contents.toString()).to.have.length.above(1);
+            });
+            return promise;
+        });
+    });
+
+
+    describe('#processEntity()', function()
+    {
+        it('should return a promise', function()
+        {
+            const testee = createTestee();
+            const promise = testee.processEntity();
+            expect(promise).to.be.instanceof(Promise);
+            return promise;
+        });
+
+        it('should call renderEntity for each export configuration', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                sinon.spy(testee, 'renderEntity');
+                const entity = (yield global.fixtures.globalRepository.resolveEntities('base/modules/m-teaser')).pop();
+                entity.properties.load({ export: { default: [{}, {}] }});
+                yield testee.processEntity(entity);
+                expect(testee.renderEntity.calledTwice).to.be.true;
+            });
+            return promise;
+        });
+
+        it('should yield a VinylFile for each export configuration', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                const entity = (yield global.fixtures.globalRepository.resolveEntities('base/modules/m-teaser')).pop();
+                entity.properties.load({ export: { default: [{}, {}] }});
+                const files = yield testee.processEntity(entity);
+                expect(files).to.be.instanceof(Array);
+                expect(files).to.have.length(2);
+            });
+            return promise;
+        });
+    });
+});
