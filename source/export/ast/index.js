e492da8e9f1f070561747ebcd54d497f68a852c2
Christian Auth <c.auth@syzygy.de>
2017-08-10 09:09:04 +0200

    fix(export.ast): adds missing index references

--- source/export/ast/index.js
+++ source/export/ast/index.js
@@ -4,4 +4,30 @@
  */
 module.exports =
 {
+    ArgumentNode: require('./ArgumentNode.js').ArgumentNode,
+    ArrayNode: require('./ArrayNode.js').ArrayNode,
+    BlockNode: require('./BlockNode.js').BlockNode,
+    BooleanOperandNode: require('./BooleanOperandNode.js').BooleanOperandNode,
+    CallableNode: require('./CallableNode.js').CallableNode,
+    CallNode: require('./CallNode.js').CallNode,
+    ComplexVariableNode: require('./ComplexVariableNode.js').ComplexVariableNode,
+    ConditionNode: require('./ConditionNode.js').ConditionNode,
+    ElseIfNode: require('./ElseIfNode.js').ElseIfNode,
+    ExpressionNode: require('./ExpressionNode.js').ExpressionNode,
+    FilterNode: require('./FilterNode.js').FilterNode,
+    ForNode: require('./ForNode.js').ForNode,
+    GroupNode: require('./GroupNode.js').GroupNode,
+    IfNode: require('./IfNode.js').IfNode,
+    LiteralNode: require('./LiteralNode.js').LiteralNode,
+    MacroNode: require('./MacroNode.js').MacroNode,
+    Node: require('./Node.js').Node,
+    NodeList: require('./NodeList.js').NodeList,
+    OperandNode: require('./OperandNode.js').OperandNode,
+    OutputNode: require('./OutputNode.js').OutputNode,
+    ParameterNode: require('./ParameterNode.js').ParameterNode,
+    SetNode: require('./SetNode.js').SetNode,
+    TextNode: require('./TextNode.js').TextNode,
+    ValueNode: require('./ValueNode.js').ValueNode,
+    VariableNode: require('./VariableNode.js').VariableNode,
+    YieldNode: require('./YieldNode.js').YieldNode
 };
