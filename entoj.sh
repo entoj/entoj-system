15757e779d3bd3b2f42560c01b263ff7712ccf30
Christian Auth <c.auth@syzygy.de>
2017-09-26 09:15:05 +0200

    chore: adds profile flag

--- entoj.sh
+++ entoj.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+# Prepare
+export FORCE_COLOR=true
+
 # Prepare pathes
 SELF=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT="$SELF/../.."
 CLI="$SELF/source/cli.js"
-COMMAND="$1"
+PROFILE="false"
 
-node $CLI "$@"
+# Prepare options
+for arg do
+  shift
+  [ "$arg" = "--profile" ] && PROFILE="true" && continue
+  set -- "$@" "$arg"
+done
+
+# Prepare runner
+if [ "$PROFILE" == "true" ]
+    then
+        echo "Enabling profiler"
+        RUNNER="node --prof $SELF/source/cli.js"
+    else
+        RUNNER="node $SELF/source/cli.js"
+fi
+
+# Doit
+$RUNNER "$@";
