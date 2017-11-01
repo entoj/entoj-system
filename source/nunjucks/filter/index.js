64d97a5bdb1e539f6ff24554afac9e6e894c9819
Christian Auth <c.auth@syzygy.de>
2017-11-01 17:22:48 +0100

    fix(nunjucks.filter): removes format numbers

--- source/nunjucks/filter/index.js
+++ source/nunjucks/filter/index.js
@@ -9,7 +9,6 @@ module.exports =
     EmptyFilter: require('./EmptyFilter.js').EmptyFilter,
     Filter: require('./Filter.js').Filter,
     FormatDateFilter: require('./FormatDateFilter.js').FormatDateFilter,
-    FormatNumberFilter: require('./FormatNumberFilter.js').FormatNumberFilter,
     GetFilter: require('./GetFilter.js').GetFilter,
     HyphenateFilter: require('./HyphenateFilter.js').HyphenateFilter,
     LinkUrlFilter: require('./LinkUrlFilter.js').LinkUrlFilter,
