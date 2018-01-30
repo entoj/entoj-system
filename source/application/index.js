ee3e9763dc3e123aa5319265036b9c4b7913e4dc
Christian Auth <c.auth@syzygy.de>
2018-01-30 21:27:05 +0100

    fix(application): adds Configuration and Communication to index

--- source/application/index.js
+++ source/application/index.js
@@ -4,6 +4,8 @@
  */
 module.exports =
 {
+    Communication: require('./Communication.js').Communication,
+    Configuration: require('./Configuration.js').Configuration,
     Context: require('./Context.js').Context,
     Runner: require('./Runner.js').Runner
 };
