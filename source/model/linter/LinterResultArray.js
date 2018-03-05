afe12e527651caf6a4271b4e1fb963daf46fa192
Christian Auth <c.auth@syzygy.de>
2018-03-05 08:55:15 +0100

    feat(model.linter.LinterResultArray): add global warningCount, errorCount and success

--- source/model/linter/LinterResultArray.js
+++ source/model/linter/LinterResultArray.js
@@ -16,14 +16,71 @@ const LinterResult = require('./LinterResult.js').LinterResult;
 class LinterResultArray extends SearchableArray
 {
     /**
-     * The namespaced class name
-     *
-     * @type {string}
-     * @static
+     * @ignore
+     */
+    constructor(...args)
+    {
+        super(...args);
+
+        // Initial values
+        this._warningCount = 0;
+        this._errorCount = 0;
+        this._success = true;
+
+        // Listen for changes
+        this.events.on('change', this.updateSummary.bind(this));
+    }
+
+
+    /**
+     * @inheritDoc
      */
     static get className()
     {
         return 'model.linter/LinterResultArray';
+    }
+
+
+    /**
+     * @protected
+     */
+    updateSummary()
+    {
+        this._warningCount = 0;
+        this._errorCount = 0;
+        for (const item of this)
+        {
+            this._warningCount+= item.warningCount;
+            this._errorCount+= item.errorCount;
+        }
+        this._success = this._warningCount === 0 && this._errorCount === 0;
+    }
+
+
+    /**
+     * @type {Number}
+     */
+    get warningCount()
+    {
+        return this._warningCount;
+    }
+
+
+    /**
+     * @type {Number}
+     */
+    get errorCount()
+    {
+        return this._errorCount;
+    }
+
+
+    /**
+     * @type {Boolean}
+     */
+    get success()
+    {
+        return this._success;
     }
 
 
