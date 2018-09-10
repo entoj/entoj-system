68822de2603e3f8ed8711a6fc23d6ae1ecdf0cba
Christian Auth <c.auth@syzygy.de>
2018-09-10 18:21:48 +0200

    chore: add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,31 @@
 module.exports = {
-    "rules": {
-        "indent": [2, 4, {"VariableDeclarator": 0, "SwitchCase": 1 }],
-        "quotes": [2, "single"],
-        "linebreak-style": [2, "unix"],
-        "semi": [2, "always"],
-        "no-var": 2,
-        "brace-style": [2, "allman"],
-        "no-unused-vars": [2, { "args": "none" }],
-        "constructor-super": 0,
-        "no-this-before-super": 0,
-        "valid-jsdoc": [0, { "requireReturnDescription": false}],
-        "prefer-const": 2,
-        "no-var": 2,
-        "no-useless-constructor": 2,
-        "no-case-declarations": 0,
-        "complexity": ["error", { "max": 40 }]
+    rules: {
+        indent: [2, 4, { VariableDeclarator: 0, SwitchCase: 1 }],
+        quotes: [2, 'single'],
+        'linebreak-style': [2, 'unix'],
+        semi: [2, 'always'],
+        'no-var': 2,
+        'no-unused-vars': [2, { args: 'none' }],
+        'constructor-super': 0,
+        'no-this-before-super': 0,
+        'valid-jsdoc': [0, { requireReturnDescription: false }],
+        'prefer-const': 2,
+        'no-var': 2,
+        'no-useless-constructor': 2,
+        'no-case-declarations': 0,
+        complexity: ['error', { max: 40 }]
     },
-    "env": {
-        "es6": true,
-        "node": true,
-        "mocha": true
+    env: {
+        es6: true,
+        node: true,
+        mocha: true
     },
-    "globals": {
-        "expect": true,
-        "ES_SOURCE": true,
-        "ES_TEST": true,
-        "ES_FIXTURES": true,
-        "PATH_SEPERATOR": true
+    globals: {
+        expect: true,
+        ES_SOURCE: true,
+        ES_TEST: true,
+        ES_FIXTURES: true,
+        PATH_SEPERATOR: true
     },
-    "extends": "eslint:recommended"
+    extends: 'eslint:recommended'
 };
