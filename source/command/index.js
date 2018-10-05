5e21f8c27bfc7814b6d533feee1196d374a602bb
Christian Auth <c.auth@syzygy.de>
2018-10-05 19:36:58 +0200

    chore: remove ConfigCommand

--- source/command/index.js
+++ source/command/index.js
@@ -3,7 +3,6 @@
  */
 module.exports = {
     Command: require('./Command.js').Command,
-    ConfigCommand: require('./ConfigCommand.js').ConfigCommand,
     ExportCommand: require('./ExportCommand.js').ExportCommand,
     LintCommand: require('./LintCommand.js').LintCommand,
     ServerCommand: require('./ServerCommand.js').ServerCommand
